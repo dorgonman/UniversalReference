e689a6932645ba3fd0739ffc12d1e087d387c41b
dorgon <dorgonman@hotmail.com>
2015-05-23 02:51:22 +0800

    finish implement rule of five

--- Tuto2_Rule_Of_Five/Source.cpp
+++ Tuto2_Rule_Of_Five/Source.cpp
@@ -26,16 +26,9 @@ THE SOFTWARE.
 //http://stackoverflow.com/questions/3734247/what-are-all-the-member-functions-created-by-compiler-for-a-class-does-that-hap
 
 #include <iostream>
-class Resource{
-//compiler generated functions:
-    //default constructor
-    //copy constructor
-    //destructor
 
-   // Resource(Resource const& kSource) = delete;
-   // Resource& operator=(Resource&& rhs) = delete;
-    //generate move constructor 
-    //generate  move assignment operator
+#define SAFE_DELETE(a) do { delete (a); (a) = NULL; } while (0)
+class Resource{
 public:
     Resource* deepCopy(){
         Resource* pResource = new Resource();
@@ -47,16 +40,16 @@ public:
 };
 
 
-class UnCopableResource : Resource{
+class UnCopableResource : public Resource{
 public:
     UnCopableResource(UnCopableResource const& kSource) = delete;
     UnCopableResource& operator=(UnCopableResource& rhs) = delete;
 public:
-    std::string m_dataUnMovable;
+    std::string m_dataUnCopable;
 };
 
 
-class UnMovableResource: Resource{
+class UnMovableResource : public Resource{
 public:
      UnMovableResource(Resource const&& kSource) = delete;
      UnMovableResource& operator=(Resource&& rhs) = delete;
@@ -68,8 +61,15 @@ public:
 class Widget{
 public:
     /** Default constructor */
-    Widget(){
-        m_pHeapStorageResource = new Resource();
+    Widget()
+        //Always use member list initialization, because it always faster than initiallize in the function body
+        //ref:
+        //http://advanced-c-plus.blogspot.tw/2010/02/advantage-of-constructor-initialization.html
+        //http://sevensavants.blogspot.tw/2010/02/c_06.html
+       /* There are two steps that takes place when Member Objects of a class are initialized inside the body of a constructor.
+        1. Member Objects are allocated a memry / constructed and are given default values by the time when the control enters body of the constructor.
+        2. Later on the actual initialization happens inside the body of the constructor i.e.user written initilization code that gets called.*/
+        : m_pHeapStorageResource(new Resource()){
     }
 
     /** Copy constructor */
@@ -80,10 +80,7 @@ public:
 
     /** Destructor */
     ~Widget(){
-        if (m_pHeapStorageResource){
-            delete m_pHeapStorageResource;
-            m_pHeapStorageResource = nullptr;
-        }
+        SAFE_DELETE(m_pHeapStorageResource);//safe release resource
     }
     /** Copy assignment operator */
     Widget& operator= (const Widget& other)
@@ -100,23 +97,25 @@ public:
     /** Move constructor */
     Widget(Widget&& other){
         //memberwiseMove(other);//oops! will call memberwiseMove(Widget& other), not our intent here
+        //member list initialization
         memberwiseMove(std::forward<Widget>(other));
     }
 
     /** Move assignment operator */
     Widget& operator= (Widget&& other)
     {
-        //memberwiseMove(other);//oops! will call memberwiseMove(Widget& other), not our intent here
-        memberwiseMove(std::forward<Widget>(other));
+        //memberwiseMove(other);//oops! will call memberwiseMove(Widget& other), other is lvalue because it has a name
+        memberwiseMove(std::forward<Widget>(other));//forward rvalue to memberwiseMove
         return *this;
     }
 
 private:
     void memberwiseDeepCopy(const Widget& other){
+    
+        SAFE_DELETE(m_pHeapStorageResource);//safe release resource
         //pointer will call assignment operator of int(memory address), means copying an int
         //But it may get some trouble because "other" object may delete m_pHeapStorageResource later
         m_pHeapStorageResource = other.m_pHeapStorageResource;
-
         //we want deep copy here, so call Resource::deepCopy
         m_pHeapStorageResource = other.m_pHeapStorageResource->deepCopy();
 
@@ -127,18 +126,28 @@ private:
 
 
     void memberwiseMove(Widget&& other){
-        m_pHeapStorageResource = other.m_pHeapStorageResource; //move
-        other.m_pHeapStorageResource = nullptr;
+        SAFE_DELETE(m_pHeapStorageResource);//safe release resource
+        std::swap(m_pHeapStorageResource, other.m_pHeapStorageResource);
+        //m_pHeapStorageResource = other.m_pHeapStorageResource; //move
+        //other.m_pHeapStorageResource = nullptr;
+
+
         //Did not benefit from move semantics,
-        //because it will call std::string's  Copy assignment operator that will deep copy whole string 
+        //because we didn't implement Move assignment operator for class Resource, so it will call it's own default Copy assignment operator(memberwiseCopy)
         m_stackStorageResource = other.m_stackStorageResource;  
         m_i = other.m_i;                                        //did not benefit from move semantics, because it as same as copying an int
     }
 
     //will call this function if you didn't use forward
     void memberwiseMove(Widget& other){
-        m_pHeapStorageResource = other.m_pHeapStorageResource; //move
-        other.m_pHeapStorageResource = nullptr;
+        SAFE_DELETE(m_pHeapStorageResource);//safe release resource
+        std::swap(m_pHeapStorageResource, other.m_pHeapStorageResource);
+        //m_pHeapStorageResource = other.m_pHeapStorageResource; //move
+        //other.m_pHeapStorageResource = nullptr;
+
+
+        m_stackStorageResource = other.m_stackStorageResource;
+        m_i = other.m_i;
     }
 public:
     Resource* m_pHeapStorageResource;
@@ -149,10 +158,20 @@ public:
 
 
 int main(){
-    using namespace std;
-    Widget origin;
-    Widget or2;
-    origin = or2;
+
+    Widget obj1;
+    Widget obj2(obj1);// call Copy constructor
+    Widget obj3;
+    obj3 = obj1; // call Copy assignment operator
+
+    Widget obj4;
+    obj4 = Widget(); //call Move assignment operator
+
+    // call Move constructor, should not call obj1 again, because we mark obj1 move to obj5 here
+    Widget obj5 = std::move(obj1);
+
+    //call Move assignment operator, should not call obj2 again, because we mark obj2 move to obj5 here
+    obj5 = std::move(obj2);
 
 
     system("pause");
